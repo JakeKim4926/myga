bbf92743b726ff2e807da0178d48f2af6fb3c6d8
Jake_Kim <wer1915@gmail.com>
2024-07-04 16:30:11 +0900

    chore: init frontend project with Vue3

---
+++ myga_front/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <TheHeader />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import TheHeader from './components/common/TheHeader.vue';
+
+</script>
+
+<style>
+
+</style>
