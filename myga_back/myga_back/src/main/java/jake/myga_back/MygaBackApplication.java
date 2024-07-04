b21f224ec44d5c9f76a25eb63415737fca99d349
Jake_Kim <wer1915@gmail.com>
2024-07-04 17:05:19 +0900

    chore: init backend project with Spring-boot

---
+++ myga_back/myga_back/src/main/java/jake/myga_back/MygaBackApplication.java
@@ -0,0 +1,13 @@
+package jake.myga_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygaBackApplication.class, args);
+	}
+
+}
---
+++ myga_back/myga_back/src/test/java/jake/myga_back/MygaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package jake.myga_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
